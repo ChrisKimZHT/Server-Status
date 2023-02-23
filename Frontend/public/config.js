a0ef5099369fa3f952deb27a49ddcba41e349588
ChrisKim <1515748548@qq.com>
2023-02-23 15:20:01 +0800

    [refactor]: 改GET请求便于CDN缓存

--- Frontend/public/config.js
+++ Frontend/public/config.js
@@ -19,7 +19,7 @@ window.Config = {
 
   // 服务状态设置
   uptimeTitle: '服务监控',
-  uptimeAPI: 'https://api.chriskim.top/uptime',
+  uptimeAPI: 'https://cdn.fc.chriskim.top/uptime',
   apiKeys: [
     'm788929911-c34f907a6baee22a878e9ade',
     'm788931826-4c77125aaadad880a8ab47d5',
@@ -30,7 +30,7 @@ window.Config = {
 
   // 证书状态设置
   certTitle: '证书监控',
-  certAPI: 'https://api.chriskim.top/cert',
+  certAPI: 'https://cdn.fc.chriskim.top/cert',
   monitorDomains: [
     'www.chriskim.cn',
     'www.zouht.com',
